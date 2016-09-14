14c17400a622a4eacdbf3ed2f1a6de518f12d75c
Konstantin Cherkashin <che.constantine@gmail.com>
2016-09-14 18:54:38 +0300

    utils checkout to symlink

--- checkouts/chains/checkouts/utils/resources/migrations-test/schematest-001.down.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE test_tbl;
-DROP TABLE test_include_tbl;
-DROP TABLE test_external_include_tbl;
--- checkouts/chains/checkouts/utils/src/utils/schema/common.clj
+++
@@ -1,6 +0,0 @@
-(ns utils.schema.common
-  (:require [schema.core :as s]))
-
-(s/defschema Settings
-  {:hello s/Str
-   (s/optional-key :name) s/Str})
